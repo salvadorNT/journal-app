d2b7a394e5ae30a98632b0b254e5ab5858e749f8
salvador <salvadorjnt@gmail.com>
2024-06-26 17:31:47 -0600

    Configure store

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,7 @@
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 const drawerWitdth = 240;
 
@@ -9,7 +10,22 @@ export const JournalPage = () => {
     <>
       <JournalLayout>
         {/* <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi atque numquam molestiae ullam et enim amet, eaque, culpa vitae sed sunt ut eligendi harum expedita veritatis, nemo fugit ratione est.</Typography> */}
-        <NoteView />
+        <NothingSelectedView />
+
+        <IconButton
+        size="large"
+        sx={{
+          color:"white",
+          backgroundColor: 'error.main',
+          ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right:50,
+          bottom: 50
+
+        }}
+        >
+          <AddOutlined sx={{fontSize:30}}/>
+        </IconButton>
       </JournalLayout>
     </>
   )
