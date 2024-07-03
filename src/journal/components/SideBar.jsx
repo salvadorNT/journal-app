b75c7a7195e5ca4b2310e816c5ea03108fc7b07f
salvador <salvadorjnt@gmail.com>
2024-07-03 11:12:49 -0600

    render display name of loged user

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,7 +1,9 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+import { useSelector } from "react-redux"
 
 export const SideBar = ({ drawerWitdth }) => {
+    const {displayName} = useSelector(state => state.auth)
     return (
         <Box
             component='nav'
@@ -17,7 +19,7 @@ export const SideBar = ({ drawerWitdth }) => {
             >
                 <Toolbar>
                     <Typography variant="h6" noWrap>
-                        Jose Salvador
+                        {displayName}
                     </Typography>
                 </Toolbar>
 
