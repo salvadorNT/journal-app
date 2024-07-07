10ed14118ecce7725a9395bd98f124f72711d935
salvador <salvadorjnt@gmail.com>
2024-07-07 00:11:05 -0600

    display loaded notes on sidebar

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,9 +1,10 @@
-import { TurnedInNot } from "@mui/icons-material"
-import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+import { Box, Divider, Drawer, Grid, List, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./";
 
 export const SideBar = ({ drawerWitdth }) => {
-    const {displayName} = useSelector(state => state.auth)
+    const { displayName } = useSelector(state => state.auth);
+    const { notes } = useSelector(state => state.journal)
     return (
         <Box
             component='nav'
@@ -26,19 +27,8 @@ export const SideBar = ({ drawerWitdth }) => {
                 <Divider />
                 <List>
                     {
-                        ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'].map( text => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TurnedInNot/>
-                                    </ListItemIcon>
-                                    <Grid container>
-                                        <ListItemText primary={text} />
-                                        <ListItemText secondary={ 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, recusandae sapiente. Architecto' }/>
-                                        
-                                    </Grid>
-                                </ListItemButton>
-                            </ListItem>
+                        notes.map(note => (
+                            <SideBarItem key={note.id} {...note} />
                         ))
                     }
                 </List>
--- src/journal/components/index.js
+++ src/journal/components/index.js
@@ -1,3 +1,4 @@
 export * from './Navbar';
 export * from './SideBar';
+export * from './SideBarItem';
 export * from './ImageGalery';
